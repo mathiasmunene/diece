cc67afbaf35ed502fbb26e10ca358a7051af65e1
mathiasmunene <mathiasmunene@gmail.com>
2025-06-20 03:21:36 +0300

    .py another commit(if std added)

--- diece.py
+++ diece.py
@@ -1,10 +1,22 @@
+import random
 #Loop
 #Ask: roll the dice?
-choice input('Roll the dice? (y/n): ')
-import random
+choice input('Roll the dice? (y/n): ').lower()
+
 
 #If user enter y
+if choice == 'y':
+    # Generate two random numbers between A and 6B
+    die1 = random.randint(1, 6)
+    die2 = random.randint(1, 6)
+    
+    # Print the results
+    print(f'You rolled a {die1} and a {die2}.')
 #  Generate two random numbers
+elif choice == 'n':
+    # Print thank you message
+    print('Thank you for playing!') 
+
 #  Print them
 #If user enters n
 #  Print thank you message
