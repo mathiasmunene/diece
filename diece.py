a70f9e54033365789ae38dc1da9d27041a1f803f
mathiasmunene <mathiasmunene@gmail.com>
2025-06-20 05:20:56 +0300

    resolved indeces problem

--- diece.py
+++ diece.py
@@ -1,15 +1,17 @@
 import random
 #Loop
 #Ask: roll the dice?
-choice = input('Roll the dice? (y/n): ').lower()
-
-
-#If user enter y
-if choice == 'y':
-    # Generate two random numbers between A and 6B
-    die1 = random.randint(1, 6)
-    die2 = random.randint(1, 6)
-    
+while True:
+    choice = input('Roll the dice? (y/n): ').lower()
+    if choice == 'y':
+        die1 = random.randint(1, 6)
+        die2 = random.randint(1, 6)
+        print(f'You rolled a {die1} and a {die2}.')
+    elif choice == 'n':
+        print('Thank you for playing!')
+        break
+    else:
+        print('Invalid choice!')
 
 #  Print them
 #If user enters n
